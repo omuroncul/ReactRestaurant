47344f63f517edc4ac510388af10c6af71366b52
omruye oncul <omuroncul@gmail.com>
2023-11-25 22:57:13 +0300

    readme

--- burger/src/App.js
+++ burger/src/App.js
@@ -6,6 +6,7 @@ import Home from "./pages/Home";
 import Menu from "./pages/Menu";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import { Margin } from "./components/Min";
 
 
 function App() {
